eecaa8f5f48bdc7ab35c0c3b6cb812151b2bf4c9
Timothy Lee <tjl97@cornell.edu>
2021-10-15 20:23:09 -0400

    Added instructions for main function in main.ml

--- main.ml
+++ main.ml
@@ -1,13 +1,31 @@
 open State
 
-(** testing*)
-let main () =
+let milk_in_recipe () =
+  print_endline "Amount of milk:"
+
+let create_recipe () = 
   ANSITerminal.print_string [ ANSITerminal.red ]
-    "\n\nWelcome to the Coffee Shop.\n";
-  print_endline
-    "Your goal is to make as much money as possible.\n";
-  print_string "> "
+    "\nStep 1: Create a Recipe for the Day\n";
+  milk_in_recipe () 
+
+let setup ()= create_recipe ()
+  
+let fail_setup () = print_endline "Oh no, there has been an error"
+ 
+
+let main () = 
+  ANSITerminal.(print_string [red] "\nWelcome to ShopTest!\n");
+  print_endline "Your goal is to make as much money as possible";
+  print_endline "For each day, you can\n- create a new coffee recipe\n- set your price for a cup of coffee\n- (re)stock your inventory";
+  print_endline "At the end of the day, you will see your profit or loss based on the consumers preferences"; 
+  print_endline "Type a number 0 to 9 and enter to start";
+  match read_line () with
+  | number -> if let x = int_of_string number in x >= 0 && x <= 9 then setup () else fail_setup ()
+
+
+let () = main () 
 
   
 
 let () = main ()
+
