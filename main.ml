e2d3add5a84aea77389e01af9eb6f70fe516064b
Timothy Lee <tjl97@cornell.edu>
2021-10-17 12:59:20 -0400

    Fixed function milk_in_recipe in main.ml

--- main.ml
+++ main.ml
@@ -1,31 +1,31 @@
 open State
 
-let milk_in_recipe () =
-  print_endline "Amount of milk:"
-
-let create_recipe () = 
+let rec milk_in_recipe x =
+  print_endline "Amount of milk:";
+  match read_line () with
+  | x when x = "quit" -> print_endline "Thanks for playing"
+  | number -> try let n = int_of_string number in 
+    if n >=0 then begin print_endline (string_of_int n) end
+    else begin print_endline "Invalid number, try again"; milk_in_recipe x end 
+  with | _ -> print_endline "Invalid input, try again"; milk_in_recipe x
+  
+let create_recipe x = 
   ANSITerminal.print_string [ ANSITerminal.red ]
     "\nStep 1: Create a Recipe for the Day\n";
-  milk_in_recipe () 
+  milk_in_recipe x
 
-let setup ()= create_recipe ()
+let start_game x = create_recipe x
   
-let fail_setup () = print_endline "Oh no, there has been an error"
- 
-
 let main () = 
   ANSITerminal.(print_string [red] "\nWelcome to ShopTest!\n");
-  print_endline "Your goal is to make as much money as possible";
+  print_endline "Your goal is to make more money than the Ai";
   print_endline "For each day, you can\n- create a new coffee recipe\n- set your price for a cup of coffee\n- (re)stock your inventory";
-  print_endline "At the end of the day, you will see your profit or loss based on the consumers preferences"; 
-  print_endline "Type a number 0 to 9 and enter to start";
+  print_endline "At the end of the day, you will see your profit or loss based on the consumers preferences";
+  print_endline "To quit the game type 'quit'"; 
+  print_endline "Type a number 1 to 3 for the difficulty of the Ai with 3 being most difficult";
   match read_line () with
-  | number -> if let x = int_of_string number in x >= 0 && x <= 9 then setup () else fail_setup ()
-
+  | number -> let x = int_of_string number in if x >= 1 && x <= 3 then start_game x 
+  else print_endline "Oh no, there has been an error setting the difficulty"
 
 let () = main () 
-
-  
-
-let () = main ()
 
