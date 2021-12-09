a1d43998bacd5b30ac7be5d19c2df13623e9f946
mrkerwinchen <kc764@cornell.edu>
2021-12-09 17:30:38 -0500

    Move random gen to separate file

--- main.ml
+++ main.ml
@@ -1,26 +1,8 @@
 open State
 open Matplotlib
+open Random_gen
 
 exception Quit of string
-
-let runif_std () =
-  let _ = Random.self_init () in
-  Random.float 1.
-
-let rec runif ~a ~b =
-  let _ = Random.self_init () in
-  let value = Random.float b in
-  if value >= a then value else runif ~a ~b
-
-let rec runif_disc ~a ~b =
-  let _ = Random.self_init () in
-  let value = Random.int (b + 1) in
-  if value >= a then value else runif_disc ~a ~b
-
-let rnorm_std () =
-  cos (2. *. Float.pi *. runif_std ()) *. sqrt (-2. *. log (runif_std ()))
-
-let rnorm ~mu ~sigma = mu +. (sigma *. rnorm_std ())
 
 let rec temp_in_recipe () =
   print_endline "Temperature of coffee: 'hot' or 'cold'";
