44828a25c4a133a7fee19005569168c7cac56612
Mary <mme59@cornell.edu>
2021-12-09 18:32:34 -0500

    home modifications

--- main.ml
+++ main.ml
@@ -318,27 +318,29 @@ let rec set_difficulty () =
 
 let main () =
   let _ = Sys.command "rm -fr reports; mkdir -p reports; clear" in
-  print_string "\n     (  )   (   )  )";
-  print_string "\n      ) (   )  (  (";
-  print_string "\n      ( )  (    ) )";
-  print_string "\n      _____________";
-  print_string "\n     <_____________> ___";
-  print_string "\n     |             |/ _ \\";
-  print_string "\n     | ";
+  print_string "\n               (  )   (   )  )";
+  print_string "\n                ) (   )  (  (";
+  print_string "\n                ( )  (    ) )";
+  print_string "\n                _____________";
+  print_string "\n               <_____________> ___";
+  print_string "\n               |             |/ _ \\";
+  print_string "\n               | ";
   ANSITerminal.(print_string [ Bold; magenta ] " Welcome to ");
   print_string "  | | |";
-  print_string "\n     | ";
+  print_string "\n               | ";
   ANSITerminal.(print_string [ Bold; magenta ] " CoffeeShop!");
   print_string "  |_| |";
-  print_string "\n     |             |\\___/";
-  print_string "\n     \\_____________/    \n";
-  print_endline "Your goal is to make more money than the Ai";
+  print_string "\n               |             |\\___/";
+  print_string "\n               \\_____________/    \n";
+  print_string "╔═══════════════════════════════════════════════╗\n";
   print_endline
-    "For each day, you can\n\
-    \  \n\
-    \     - create a new coffee recipe\n\
-     - set your price for a cup of coffee\n\
-     - (re)stock your inventory";
+    "║ Your goal is to make more money than the AI.  ║\n\
+     ║ For each day, you can:                        ║\n\
+     ║ - create a new coffee recipe                  ║\n\
+     ║ - set your price for a cup of coffee          ║\n\
+     ║ - (re)stock your inventory                    ║\n\
+     ╚═══════════════════════════════════════════════╝";
+
   print_endline
     "At the end of the day, you will see your profit or loss based on the \
      consumers preferences";
