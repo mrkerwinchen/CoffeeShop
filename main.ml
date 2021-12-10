336e0bef034dc163e9aceeb6e7183cde914f539e
Mary <mme59@cornell.edu>
2021-12-09 21:18:37 -0500

    wrote coffee ux file and matplotlib works on mac now

--- main.ml
+++ main.ml
@@ -1,5 +1,6 @@
 open State
 open Matplotlib
+open Coffeeux
 open Random_gen
 
 exception Quit of string
@@ -300,32 +301,7 @@ let rec set_difficulty () =
 
 let main () =
   let _ = Sys.command "rm -fr reports; mkdir -p reports; clear" in
-  print_string "\n               (  )   (   )  )";
-  print_string "\n                ) (   )  (  (";
-  print_string "\n                ( )  (    ) )";
-  print_string "\n                _____________";
-  print_string "\n               <_____________> ___";
-  print_string "\n               |             |/ _ \\";
-  print_string "\n               | ";
-  ANSITerminal.(print_string [ Bold; magenta ] " Welcome to ");
-  print_string "  | | |";
-  print_string "\n               | ";
-  ANSITerminal.(print_string [ Bold; magenta ] " CoffeeShop!");
-  print_string "  |_| |";
-  print_string "\n               |             |\\___/";
-  print_string "\n               \\_____________/    \n";
-  print_string "╔═════════════════════════════════════════════════╗\n";
-  print_endline
-    "║ Your goal is to make more money than the AI.    ║\n\
-     ║ For each day, you can:                          ║\n\
-     ║ - create a new coffee recipe                    ║\n\
-     ║ - set your price for a cup of coffee            ║\n\
-     ║ - (re)stock your inventory                      ║";
-
-  print_endline
-    "║ At the end of the day, you will see your profit ║ \n\
-     ║ or loss based on the consumers preferences      ║";
-  print_endline "╚═════════════════════════════════════════════════╝";
+  let _ = coffeecup () in
   ANSITerminal.(
     print_string [ magenta ]
       "To quit the game type 'quit' or ctrl + c at anytime\n");
