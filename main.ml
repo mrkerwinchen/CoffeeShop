2491f16ae21acbf4ed0e7e2bd3095e30c8269ad2
Mary <mme59@cornell.edu>
2021-12-09 19:04:15 -0500

    formatting start

--- main.ml
+++ main.ml
@@ -314,19 +314,21 @@ let main () =
   print_string "  |_| |";
   print_string "\n               |             |\\___/";
   print_string "\n               \\_____________/    \n";
-  print_string "╔═══════════════════════════════════════════════╗\n";
+  print_string "╔═════════════════════════════════════════════════╗\n";
   print_endline
-    "║ Your goal is to make more money than the AI.  ║\n\
-     ║ For each day, you can:                        ║\n\
-     ║ - create a new coffee recipe                  ║\n\
-     ║ - set your price for a cup of coffee          ║\n\
-     ║ - (re)stock your inventory                    ║\n\
-     ╚═══════════════════════════════════════════════╝";
+    "║ Your goal is to make more money than the AI.    ║\n\
+     ║ For each day, you can:                          ║\n\
+     ║ - create a new coffee recipe                    ║\n\
+     ║ - set your price for a cup of coffee            ║\n\
+     ║ - (re)stock your inventory                      ║";
 
   print_endline
-    "At the end of the day, you will see your profit or loss based on the \
-     consumers preferences";
-  ANSITerminal.(print_string [ magenta ] "To quit the game type 'quit'\n");
+    "║ At the end of the day, you will see your profit ║ \n\
+     ║ or loss based on the consumers preferences      ║";
+  print_endline "╚═════════════════════════════════════════════════╝";
+  ANSITerminal.(
+    print_string [ magenta ]
+      "To quit the game type 'quit' or ctrl + c at anytime\n");
   let _ = set_difficulty () in
   () |> initialize_state |> start_game
 
