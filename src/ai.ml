f10964282c093f16c68d49c3831cc7639517bb00
Timothy Lee <tjl97@cornell.edu>
2021-12-12 17:07:20 -0500

    ai medium

--- src/ai.ml
+++ src/ai.ml
@@ -18,6 +18,7 @@ type ai_state = {
   ai : int;
   ai_revenue : float array;
   ai_temperature : float;
+  ai_previous_inventory : inventory;
 }
 
 let int_to_difficulty ai =
@@ -93,10 +94,50 @@ let medium_init_recipe temp : coffee =
       temp = Hot;
     }
   in
+  let temp_runif = rnorm ~mu:50. ~sigma:10. in
+  if temp > temp_runif then { recipe with temp = Cold } else recipe
+
+let hard_init_recipe temp =
+  let recipe = { milk = 4; sugar = 4; beans = 4; price = 3.5; temp = Hot } in
   if temp > 50. then { recipe with temp = Cold } else recipe
 
-let hard_init_recipe : coffee =
-  { milk = 4; sugar = 4; beans = 4; price = 3.5; temp = Hot }
+let lst_rec_by_ingr (recipe : coffee) =
+  let milk = recipe.milk in
+  let sugar = recipe.sugar in
+  let beans = recipe.beans in
+  let lst = [ ("milk", milk); ("sugar", sugar); ("beans", beans) ] in
+  List.sort (fun (_, a) (_, b) -> compare a b) lst
+
+let incr_ingr_recipe ingr num_units (recipe : coffee) =
+  match ingr with
+  | "milk" -> { recipe with milk = recipe.milk + num_units }
+  | "sugar" -> { recipe with sugar = recipe.sugar + num_units }
+  | "beans" -> { recipe with beans = recipe.beans + num_units }
+  | _ -> raise (Failure "Not ingredient")
+
+let incr_all_ingr_recipe (recipe : coffee) num_units =
+  {
+    recipe with
+    milk = recipe.milk + num_units;
+    sugar = recipe.sugar + num_units;
+    beans = recipe.beans + num_units;
+  }
+
+let medium_recipe_price (ai_state : ai_state) prices =
+  let recipe = ai_state.ai_recipe in
+  let cost_one_coff = cost_of_one_coffee ai_state prices in
+  let price = cost_one_coff +. (1.25 *. cost_one_coff) in
+  { recipe with price }
+
+let rec medium_incr_recipe recipe diff_lst =
+  match diff_lst with
+  | [] -> recipe
+  | [ h ] -> recipe
+  | (ingr1, diff1) :: (ingr2, diff2) :: t ->
+      if diff2 - diff1 > 0 || diff1 <= 4 then
+        let new_rec = incr_ingr_recipe ingr1 1 recipe in
+        medium_incr_recipe new_rec ((ingr2, diff2) :: t)
+      else medium_incr_recipe recipe ((ingr2, diff2) :: t)
 
 let easy_create_recipe ai_state prices =
   let old_recipe = easy_init_recipe in
@@ -104,7 +145,24 @@ let easy_create_recipe ai_state prices =
   let new_price = cost_of_one_coffee ai_state prices +. r_profit_cup in
   { old_recipe with price = new_price }
 
-let medium_create_recipe = "a"
+let medium_create_ingr_recipe ai_state prices =
+  let prev_rec = ai_state.ai_recipe in
+  let prev_inv = ai_state.ai_previous_inventory in
+  let inv = ai_state.ai_inventory in
+  let max_coff_can_sell = max_coffee_can_sell ai_state prices in
+  let coff_sold = prev_inv.cups - inv.cups in
+  let diff_from_max_sold = max_coff_can_sell - coff_sold in
+  if diff_from_max_sold = 0 then incr_all_ingr_recipe prev_rec 1
+  else
+    let lst_rec = lst_rec_by_ingr prev_rec in
+    medium_incr_recipe prev_rec lst_rec
+
+let medium_create_recipe ai_state prices =
+  let new_rec = medium_create_ingr_recipe ai_state prices in
+  let new_st = { ai_state with ai_recipe = new_rec } in
+  let new_rec = medium_recipe_price new_st prices in
+  if ai_state.ai_temperature < 50. then { new_rec with temp = Hot }
+  else { new_rec with temp = Cold }
 
 let hard_create_recipe = hard_init_recipe
 
@@ -121,28 +179,36 @@ let can_buy_ingr (inventory : inventory) (ingr : string) prices num_units =
 let buy_idv_units (inv : inventory) (ingr : string) prices num_units =
   match ingr with
   | "milk" ->
+      let cost = float_of_int num_units *. prices.milk in
       {
         inv with
         milk = inv.milk + num_units;
-        cash = inv.cash -. (float_of_int num_units *. prices.milk);
+        cash = inv.cash -. cost;
+        total_expense = inv.total_expense +. cost;
       }
   | "sugar" ->
+      let cost = float_of_int num_units *. prices.sugar in
       {
         inv with
         sugar = inv.sugar + num_units;
-        cash = inv.cash -. (float_of_int num_units *. prices.sugar);
+        cash = inv.cash -. cost;
+        total_expense = inv.total_expense +. cost;
       }
   | "beans" ->
+      let cost = float_of_int num_units *. prices.beans in
       {
         inv with
         beans = inv.beans + num_units;
-        cash = inv.cash -. (float_of_int num_units *. prices.beans);
+        cash = inv.cash -. cost;
+        total_expense = inv.total_expense +. cost;
       }
   | "cups" ->
+      let cost = float_of_int num_units *. prices.cups in
       {
         inv with
         cups = inv.cups + num_units;
-        cash = inv.cash -. (float_of_int num_units *. prices.cups);
+        cash = inv.cash -. cost;
+        total_expense = inv.total_expense +. cost;
       }
   | _ -> raise (Failure "Not ingredient")
 
@@ -186,38 +252,34 @@ let medium_fill_inventory (ai_state : ai_state) prices =
 let hard_fill_inventory (ai_state : ai_state) prices =
   buy_all_ingr ai_state prices
 
+let ai_init_inventory =
+  { milk = 0; sugar = 0; beans = 0; cups = 0; cash = 100.; total_expense = 0. }
+
 (**[ai_init_recipe] is ai first recipe choosen by distributions*)
 let ai_init_state diff : ai_state =
   {
     ai_day = 0;
     ai_recipe = { milk = 0; sugar = 0; beans = 0; price = 0.; temp = Hot };
-    ai_inventory =
-      {
-        milk = 0;
-        sugar = 0;
-        beans = 0;
-        cups = 0;
-        cash = 100.;
-        total_expense = 0.;
-      };
+    ai_inventory = ai_init_inventory;
     ai_customers = [||];
     ai = diff;
     ai_revenue = [||];
     ai_temperature = 0.;
+    ai_previous_inventory = ai_init_inventory;
   }
 
 let ai_init_recipe (ai_state : ai_state) =
   match int_to_difficulty ai_state.ai with
   | Easy -> easy_init_recipe
   | Medium -> medium_init_recipe ai_state.ai_temperature
-  | Hard -> hard_init_recipe
+  | Hard -> hard_init_recipe ai_state.ai_temperature
 
 (**[ai_create_recipe] is the recipe ai chooses for the day*)
 let ai_create_recipe (ai_state : ai_state) prices =
   match int_to_difficulty ai_state.ai with
   | Easy -> easy_create_recipe ai_state prices
-  | Medium -> raise (Failure "Implement")
-  | Hard -> hard_create_recipe
+  | Medium -> medium_create_recipe ai_state prices
+  | Hard -> hard_create_recipe ai_state.ai_temperature
 
 (**[ai_fill_inventory] is the how ai restocks the inventory for the day*)
 let ai_fill_inventory (ai_state : ai_state) prices =
@@ -240,6 +302,7 @@ let ai_pre_day (ai_state : ai_state) prices (temp : float) =
     ai_inventory = new_inv;
     ai_customers = customers;
     ai_temperature = temp;
+    ai_previous_inventory = new_inv;
   }
 
 let ai_enough_supplies (ai_state : ai_state) : bool =
@@ -253,12 +316,12 @@ let ai_purchase_coffee (ai_state : ai_state) one_coffee_cost money_gained =
     ai_state with
     ai_inventory =
       {
-        ai_state.ai_inventory with
         milk = ai_state.ai_inventory.milk - ai_state.ai_recipe.milk;
         beans = ai_state.ai_inventory.beans - ai_state.ai_recipe.beans;
         sugar = ai_state.ai_inventory.sugar - ai_state.ai_recipe.sugar;
         cups = ai_state.ai_inventory.cups - 1;
         cash = ai_state.ai_inventory.cash -. one_coffee_cost +. money_gained;
+        total_expense = ai_state.ai_inventory.total_expense -. money_gained;
       };
   }
 
